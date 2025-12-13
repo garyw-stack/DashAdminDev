9fb211f302357f55c380480008fc017390dbb39a
garyw-stack <gary.wilson@funatthetrack.com>
2025-12-13 14:25:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
  // Edit Below 
    const footerText = `
-   Version history last updated: <strong>December 12, 2025</strong> • Maintained with care
+   Version history last updated: <strong>December 13, 2025</strong> • Maintained with care
     `;
 
   container.innerHTML = `
@@ -137,3 +137,4 @@ document.addEventListener('DOMContentLoaded', () => {
    </footer>
    `;
   });
+
